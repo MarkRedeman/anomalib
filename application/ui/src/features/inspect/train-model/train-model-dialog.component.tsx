479d98db77a471ffb1a3efba2369d10ec470df53
Mark Redeman <markredeman@gmail.com>
2025-10-20 23:04:37 +0200

    Add train model dialog button

---
+++ application/ui/src/features/inspect/train-model/train-model-dialog.component.tsx
@@ -0,0 +1,71 @@
+import { Suspense, useState } from 'react';
+
+import { $api } from '@geti-inspect/api';
+import { useProjectIdentifier } from '@geti-inspect/hooks';
+import { Button, ButtonGroup, Content, Dialog, Divider, Heading, Loading, RadioGroup, View } from '@geti/ui';
+
+import { TrainableModelListBox } from './trainable-model-list-box.component';
+
+import classes from './train-model.module.scss';
+
+export const TrainModelDialog = ({ close }: { close: () => void }) => {
+    const { projectId } = useProjectIdentifier();
+    const startTraining = async () => {
+        if (selectedModel === null) {
+            return;
+        }
+
+        await startTrainingMutation.mutateAsync({
+            body: { project_id: projectId, model_name: selectedModel },
+        });
+
+        close();
+    };
+    const [selectedModel, setSelectedModel] = useState<string | null>(null);
+    const startTrainingMutation = $api.useMutation('post', '/api/jobs:train');
+
+    return (
+        <Dialog size='L' UNSAFE_style={{ width: 'fit-content' }}>
+            <Heading>Train model</Heading>
+            <Divider />
+            <Content UNSAFE_style={{ width: 'fit-content' }}>
+                <View
+                    padding={'size-250'}
+                    backgroundColor={'gray-50'}
+                    flex={1}
+                    minHeight={0}
+                    overflow={'hidden auto'}
+                    minWidth={'60vw'}
+                >
+                    <RadioGroup
+                        isEmphasized
+                        aria-label={`Select a model to train`}
+                        onChange={(modelId) => {
+                            setSelectedModel(modelId);
+                        }}
+                        value={selectedModel}
+                        minWidth={0}
+                        width='100%'
+                        UNSAFE_className={classes.radioGroup}
+                    >
+                        <Suspense fallback={<Loading mode='inline' />}>
+                            <TrainableModelListBox
+                                selectedModelTemplateId={selectedModel}
+                                onChangeSelectedTemplateId={(id) => id && setSelectedModel(id)}
+                                activeModelTemplateId={null}
+                            />
+                        </Suspense>
+                    </RadioGroup>
+                </View>
+            </Content>
+            <ButtonGroup>
+                <Button variant='secondary' onPress={close}>
+                    Cancel
+                </Button>
+                <Button variant='accent' onPress={startTraining} autoFocus isPending={startTrainingMutation.isPending}>
+                    Start
+                </Button>
+            </ButtonGroup>
+        </Dialog>
+    );
+};
